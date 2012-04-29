1efd95bfaa428d4564fe6e6efaf7052de6488ef3
Adrian Sampson <adrian@radbox.org>
2012-04-28 20:33:19 -0700

    both executors inherit from a common base
    I haven't tested the new Condor executor because I no longer have a Condor
    cluster, but it should work. We now use a much dumber method for determining
    whether a job has finished (dumber but infrastructure-independent), but that
    *should* be the only substantial change to how it works.

--- cfut/__init__.py
+++ cfut/__init__.py
@@ -15,78 +15,6 @@ LOGFILE_FMT = 'cfut.log.%s.txt'
 class RemoteException(object):
     pass
 
-class CondorExecutor(futures.Executor):
-    """Futures executor for executing jobs on a Condor cluster."""
-    def __init__(self, debug=False):
-        self.debug = debug
-
-        self.logfile = LOGFILE_FMT % random_string()
-        self.jobs = {}
-        self.jobs_lock = threading.Lock()
-        self.jobs_empty_cond = threading.Condition(self.jobs_lock)
-
-        self.wait_thread = condor.WaitThread(self._completion, self.logfile)
-        self.wait_thread.start()
-
-    def _completion(self, jobid):
-        """Called whenever a job finishes."""
-        with self.jobs_lock:
-            fut, workerid = self.jobs.pop(jobid)
-            if not self.jobs:
-                self.jobs_empty_cond.notify_all()
-        if self.debug:
-            print >>sys.stderr, "job completed: %i" % jobid
-
-        with open(OUTFILE_FMT % workerid) as f:
-            outdata = f.read()
-        success, result = serialization.deserialize(outdata)
-
-        if success:
-            fut.set_result(result)
-        else:
-            fut.set_exception(RemoteException(result))
-
-        # Clean up communication files.
-        os.unlink(INFILE_FMT % workerid)
-        os.unlink(OUTFILE_FMT % workerid)
-        # Clean up Condor stream files.
-        os.unlink(condor.OUTFILE_FMT % str(jobid))
-        os.unlink(condor.ERRFILE_FMT % str(jobid))
-    
-    def submit(self, fun, *args, **kwargs):
-        """Submit a job to the pool."""
-        fut = futures.Future()
-
-        # Start the job.
-        workerid = random_string()
-        funcser = serialization.serialize((fun, args, kwargs), True)
-        with open(INFILE_FMT % workerid, 'w') as f:
-            f.write(funcser)
-        jobid = condor.submit(sys.executable, '-m cfut.remote %s' % workerid,
-                              log=self.logfile)
-
-        if self.debug:
-            print >>sys.stderr, "job submitted: %i" % jobid
-
-        # Thread will wait for it to finish.
-        self.wait_thread.wait(jobid)
-
-        with self.jobs_lock:
-            self.jobs[jobid] = (fut, workerid)
-        return fut
-
-    def shutdown(self, wait=True):
-        """Close the pool."""
-        if wait:
-            with self.jobs_lock:
-                if self.jobs:
-                    self.jobs_empty_cond.wait()
-
-        self.wait_thread.stop()
-        self.wait_thread.join()
-        if os.path.exists(self.logfile):
-            os.unlink(self.logfile)
-
 class FileWaitThread(threading.Thread):
     """A thread that polls the filesystem waiting for a list of files to
     be created. When a specified file is created, it invokes a callback.
@@ -129,8 +57,9 @@ class FileWaitThread(threading.Thread):
 
             time.sleep(self.interval)
 
-class SlurmExecutor(futures.Executor):
-    """Futures executor for executing jobs on a Slurm cluster."""
+class ClusterExecutor(futures.Executor):
+    """An abstract base class for executors that run jobs on clusters.
+    """
     def __init__(self, debug=False):
         self.debug = debug
 
@@ -141,6 +70,18 @@ class SlurmExecutor(futures.Executor):
 
         self.wait_thread = FileWaitThread(self._completion)
         self.wait_thread.start()
+
+    def _start(workerid):
+        """Start a job with the given worker ID and return an ID
+        identifying the new job. The job should run ``python -m
+        cfut.remote <workerid>.
+        """
+        raise NotImplementedError()
+
+    def _cleanup(jobid):
+        """Given a job ID as returned by _start, perform any necessary
+        cleanup after the job has finished.
+        """
 
     def _completion(self, jobid):
         """Called whenever a job finishes."""
@@ -163,9 +104,9 @@ class SlurmExecutor(futures.Executor):
         # Clean up communication files.
         os.unlink(INFILE_FMT % workerid)
         os.unlink(OUTFILE_FMT % workerid)
-        # Clean up stream files.
-        os.unlink(slurm.OUTFILE_FMT.format(str(jobid)))
-    
+
+        self._cleanup(jobid)
+
     def submit(self, fun, *args, **kwargs):
         """Submit a job to the pool."""
         fut = futures.Future()
@@ -175,9 +116,7 @@ class SlurmExecutor(futures.Executor):
         funcser = serialization.serialize((fun, args, kwargs), True)
         with open(INFILE_FMT % workerid, 'w') as f:
             f.write(funcser)
-        jobid = slurm.submit(
-            '{} -m cfut.remote {}'.format(sys.executable, workerid)
-        )
+        jobid = self._start(workerid)
 
         if self.debug:
             print >>sys.stderr, "job submitted: %i" % jobid
@@ -198,6 +137,35 @@ class SlurmExecutor(futures.Executor):
 
         self.wait_thread.stop()
         self.wait_thread.join()
+
+class SlurmExecutor(ClusterExecutor):
+    """Futures executor for executing jobs on a Slurm cluster."""
+    def _start(self, workerid):
+        return slurm.submit(
+            '{} -m cfut.remote {}'.format(sys.executable, workerid)
+        )
+
+    def _cleanup(self, jobid):
+        os.unlink(slurm.OUTFILE_FMT.format(str(jobid)))
+
+class CondorExecutor(ClusterExecutor):
+    """Futures executor for executing jobs on a Condor cluster."""
+    def __init__(self, debug=False):
+        super(CondorExecutor, self).__init__(debug)
+        self.logfile = LOGFILE_FMT % random_string()
+
+    def _start(self, workerid):
+        return condor.submit(sys.executable, '-m cfut.remote %s' % workerid,
+                             log=self.logfile)
+
+    def _cleanup(self, jobid):
+        os.unlink(condor.OUTFILE_FMT % str(jobid))
+        os.unlink(condor.ERRFILE_FMT % str(jobid))
+
+    def shutdown(self, wait=True):
+        super(CondorExecutor, self).shutdown(wait)
+        if os.path.exists(self.logfile):
+            os.unlink(self.logfile)
 
 def map(executor, func, args, ordered=True):
     """Convenience function to map a function over cluster jobs. Given
