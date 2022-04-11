cd2d0125735e160e4035ae3e47c1409e99f2289f
Thomas Kluyver <thomas@kluyver.me.uk>
2022-04-11 16:45:13 +0100

    Mock out squeue in Slurm tests

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -8,14 +8,15 @@ def square(n):
 
 
 def test_submit():
-    with cfut.SlurmExecutor(True, keep_logs=True) as executor:
-        with MockCommand.fixed_output('sbatch', stdout='000000') as sbatch:
-            fut = executor.submit(square, 2)
-        sbatch.assert_called()
+    with MockCommand.fixed_output('squeue', stdout='000000 RUNNING'):
+        with cfut.SlurmExecutor(True, keep_logs=True) as executor:
+            with MockCommand.fixed_output('sbatch', stdout='000000') as sbatch:
+                fut = executor.submit(square, 2)
+            sbatch.assert_called()
 
-        assert not fut.done()
-        run_all_outstanding_work()
-        assert fut.result(timeout=3) == 4
+            assert not fut.done()
+            run_all_outstanding_work()
+            assert fut.result(timeout=3) == 4
 
 SBATCH_JOB_COUNT = """
 from pathlib import Path
@@ -28,11 +29,19 @@ counter_file.write_text(str(count))
 print(count)
 """
 
-def test_map():
-    with cfut.SlurmExecutor(True, keep_logs=True) as executor:
-        with MockCommand('sbatch', python=SBATCH_JOB_COUNT) as sbatch:
-            result_iter = executor.map(square, range(4), timeout=5)
-        sbatch.assert_called()
+SQUEUE_STILL_GOING = """
+jobs_arg_ix = sys.argv.index('--jobs') + 1
+job_ids = sys.argv[jobs_arg_ix].split(',')
+for jid in job_ids:
+    print(jid, 'RUNNING')
+"""
 
-        run_all_outstanding_work()
-        assert list(result_iter) == [0, 1, 4, 9]
+def test_map():
+    with MockCommand('squeue', python=SQUEUE_STILL_GOING):
+        with cfut.SlurmExecutor(True, keep_logs=True) as executor:
+            with MockCommand('sbatch', python=SBATCH_JOB_COUNT) as sbatch:
+                result_iter = executor.map(square, range(4), timeout=5)
+            sbatch.assert_called()
+
+            run_all_outstanding_work()
+            assert list(result_iter) == [0, 1, 4, 9]
